e1234a03a35ac2a5e4baf9d0a774e240bec3f453
CodyConck <codyconck@gmail.com>
2023-03-21 15:17:45 -0500

    fetching data for golf news but need to export the function to news.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import News from "./pages/News";
 
 import "./App.css";
 
+//this works but needs to be in news.js and exported somehow to click event
+async function fetchGolfNews() {
+  const URL = `https://api.sportsdata.io/golf/v2/json/News?key=24c69ca6a3d246ef91da0ffe95be330a`;
+  const response = await fetch(`${URL}`);
+  const newsData = await response.json();
+  console.log(newsData);
+}
+
 function App() {
   return (
     <div>
@@ -12,7 +20,12 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/news" element={<News />}></Route>
+          <Route
+            path="/news"
+            element={<News />}
+            //thi onClick below doesnt work
+            onClick={fetchGolfNews()}
+          ></Route>
         </Routes>
       </div>
     </div>
--- src/pages/News.js
+++ src/pages/News.js
@@ -2,6 +2,13 @@ import React from "react";
 
 const News = () => {
   return <div>Will Get this linked to golf api for news</div>;
+  // async function fetchGolfNews() {
+  //   const URL = `https://api.sportsdata.io/golf/v2/json/News?key=24c69ca6a3d246ef91da0ffe95be330a`;
+  //   const response = await fetch(`${URL}`);
+  //   const newsData = await response.json();
+  //   console.log(newsData);
+  // }
+  // fetchGolfNews();
 };
 
 export default News;
